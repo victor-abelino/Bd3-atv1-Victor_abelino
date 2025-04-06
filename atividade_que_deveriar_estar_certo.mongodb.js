03b27f4bccc0fa93ab996fca7986fcdeb6833d63
VICTOR SILVA <victor.abelino7@gmail.com>
2025-04-06 11:05:39 -0300

    Criando banco e Acessando Dados

---
+++ atividade_que_deveriar_estar_certo.mongodb.js
@@ -0,0 +1,128 @@
+// use("BD3NoSQL-AtlasMongoDB");
+
+// db['BD3-nosql-atv1'].find()
+
+// db["BD3-nosql-atv1"].findOne(
+//     { cpf: "123-456-789-10" },
+//     { cod_aluno: 0 }
+//   )
+  
+// db["BD3-nosql-atv1"].findOne(
+//     { cpf: "123-456-789-10" },
+//     { cod_aluno: 0, codigo: 0 }
+//   )
+       
+  
+//   db["BD3-nosql-atv1"].insertMany([
+//     {
+//       cod_aluno: 1,
+//       cod_turma: 1,
+//       nome: 'Arlindo',
+//       cpf: '123-456-789-10',
+//       rg: '12-345-678-9',
+//       telefone_aluno: '12-3456-7891',
+//       telefone_responsavel: '19-8765-4321',
+//       email: 'arlindo@gmail.com',
+//       data_nasc: '2000-01-01'
+//     },
+//     {
+//       cod_aluno: 2,
+//       cod_turma: 2,
+//       nome: 'Beatriz',
+//       cpf: '234-567-890-11',
+//       rg: '23-456-789-0',
+//       telefone_aluno: '13-9876-5432',
+//       telefone_responsavel: '18-7654-3210',
+//       email: 'beatriz@email.com',
+//       data_nasc: '2001-02-02'
+//     },
+//     {
+//       cod_aluno: 3,
+//       cod_turma: 1,
+//       nome: 'Carlos',
+//       cpf: '345-678-901-12',
+//       rg: '34-567-890-1',
+//       telefone_aluno: '14-1234-5678',
+//       telefone_responsavel: '17-6543-2109',
+//       email: 'carlos@email.com',
+//       data_nasc: '2002-03-03'
+//     },
+//     {
+//       cod_aluno: 4,
+//       cod_turma: 3,
+//       nome: 'Daniela',
+//       cpf: '456-789-012-13',
+//       rg: '45-678-901-2',
+//       telefone_aluno: '15-2345-6789',
+//       telefone_responsavel: '16-5432-1098',
+//       email: 'daniela@email.com',
+//       data_nasc: '2003-04-04'
+//     },
+//     {
+//       cod_aluno: 5,
+//       cod_turma: 2,
+//       nome: 'Eduardo',
+//       cpf: '567-890-123-14',
+//       rg: '56-789-012-3',
+//       telefone_aluno: '16-3456-7890',
+//       telefone_responsavel: '15-4321-0987',
+//       email: 'eduardo@email.com',
+//       data_nasc: '2004-05-05'
+//     },
+//     {
+//       cod_aluno: 6,
+//       cod_turma: 3,
+//       nome: 'Fernanda',
+//       cpf: '678-901-234-15',
+//       rg: '67-890-123-4',
+//       telefone_aluno: '17-4567-8901',
+//       telefone_responsavel: '14-3210-9876',
+//       email: 'fernanda@email.com',
+//       data_nasc: '2005-06-06'
+//     },
+//     {
+//       cod_aluno: 7,
+//       cod_turma: 1,
+//       nome: 'Gabriel',
+//       cpf: '789-012-345-16',
+//       rg: '78-901-234-5',
+//       telefone_aluno: '18-5678-9012',
+//       telefone_responsavel: '13-2109-8765',
+//       email: 'gabriel@email.com',
+//       data_nasc: '2006-07-07'
+//     },
+//     {
+//       cod_aluno: 8,
+//       cod_turma: 2,
+//       nome: 'Helena',
+//       cpf: '890-123-456-17',
+//       rg: '89-012-345-6',
+//       telefone_aluno: '19-6789-0123',
+//       telefone_responsavel: '12-1098-7654',
+//       email: 'helena@email.com',
+//       data_nasc: '2007-08-08'
+//     },
+//     {
+//       cod_aluno: 9,
+//       cod_turma: 3,
+//       nome: 'Igor',
+//       cpf: '901-234-567-18',
+//       rg: '90-123-456-7',
+//       telefone_aluno: '11-7890-1234',
+//       telefone_responsavel: '11-0987-6543',
+//       email: 'igor@email.com',
+//       data_nasc: '2008-09-09'
+//     },
+//     {
+//       cod_aluno: 10,
+//       cod_turma: 1,
+//       nome: 'Juliana',
+//       cpf: '012-345-678-19',
+//       rg: '01-234-567-8',
+//       telefone_aluno: '12-8901-2345',
+//       telefone_responsavel: '10-9876-5432',
+//       email: 'juliana@email.com',
+//       data_nasc: '2009-10-10'
+//     }
+//   ]);
+  
